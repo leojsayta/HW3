2482b00a6865c7d290ab5c3665777227b34cca8b
Joel Atyas <jatyas@gmail.com>
2015-02-25 11:20:17 -0500

    Fleshing out classes with members and constructors: -Card -Hand -HandValue Implementing operator functions for enums: -++

--- HW3Classes.cpp
+++ HW3Classes.cpp
@@ -10,14 +10,50 @@
 
 Card::Card()
 {
-    
     this->Suit = CardSuit::Club;
-    this->Value = CardValue::Ace;
-    
+    this->Value = CardValue::Ace;   
+}
+
+Card::Card(CardSuit suit, CardValue val) : Card()
+{
+    this->Suit = suit;
+    this->Value = val;
+}
+
+HandValue::HandValue()
+{
+    this->TypeOfHand = HandType::HighCard;
+    this->HighCard1 = Card();
+    this->HighCard2 = Card();
+}
+
+HandValue::HandValue(const Hand* hnd) : HandValue()
+{
+    this->p_hand = hnd;
 }
 
 Hand::Hand()
 {
     this->cards = vector<Card>(5);
     
+    for(Card& cd : cards)
+    {
+        cd.Suit++;
+        cd.Value++;
+    }
+}
+
+Hand::Hand(vector<Card>& cards) : Hand()
+{
+    this->SetCards(cards);
+}
+
+constexpr CardSuit operator++(CardSuit& cs, int)
+{   
+    return cs = (cs == CardSuit::Spade) ? CardSuit::Club : static_cast<CardSuit>(static_cast<int>(cs) + 1);
+}
+
+constexpr CardValue operator++(CardValue& cv, int)
+{
+   return cv = (cv == CardValue::Ace) ? CardValue::Two : static_cast<CardValue>(static_cast<int>(cv) + 1);
 }
--- HW3Classes.h
+++ HW3Classes.h
@@ -16,7 +16,22 @@
 using namespace std;
 
 enum class CardSuit {Club = 0, Diamond, Heart, Spade};
+constexpr CardSuit operator++(CardSuit& a, int);
+
 enum class CardValue {Two = 0, Three, Four, Five, Six, Seven, Eight, Nine, Jack, Queen, King, Ace};
+constexpr CardValue operator++(CardValue& cv, int);
+
+enum class HandType {
+    HighCard = 0,
+    OnePair,
+    TwoPair,
+    Three_of_Kind,
+    Straight,
+    Flush,
+    FullHouse,
+    Four_of_Kind,
+    StraightFlush
+};
 
 class Card
 {
@@ -32,19 +47,75 @@ public:
     
     Card();
     
+    Card(CardSuit suit, CardValue val);
+};
+
+class Hand;
+
+class HandValue
+{
+private:
+
+    const Hand* p_hand = nullptr;
+    
+    HandValue();
+
+public:
+
+    HandType TypeOfHand;
+
+    /*
+     * highCard1: For 1st pair or 3 of a kind or 4 of a kind or
+     * straight or flush or straight flush.
+     * highCard2: For 2nd pair or lower pair of full house, etc.
+    */
+    Card HighCard1; 
+    Card HighCard2;
+
+    HandValue(const Hand* hnd);
+
 };
 
 class Hand
-{
+{   
     
 private:
     
     vector<Card> cards;
+    HandValue* p_handValue = nullptr;
     
 public:
     
     Hand();
     
+    Hand(vector<Card>& cards);
+
+    void SetCards(vector<Card>& cards) {
+        this->cards = cards;
+    }
+
+    vector<Card> GetCards() const {
+        return cards;
+    }
+    
+    void SortByValue();
+    
+    void SortBySuit();
+    
+    bool IsFlush();
+    
+    bool operator==(const Hand& otherHand); 
+    
+    bool operator!=(const Hand& otherHand); 
+    
+    bool operator<(const Hand& otherHand); 
+    
+    bool operator>(const Hand& otherHand);
+    
 };
+
+
+constexpr CardSuit operator++(CardSuit a); 
+
 
 #endif /* defined(__HW3__HW3Classes__) */
